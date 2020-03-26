56eeb8da08c7704f2696febbaa6fbdb275f722ee
Régis LC <regis.lecoz@ariadnext.com>
2020-03-26 19:02:11 +0100

    idToken

--- src/main/java/bzh.redge.keycloak/ApisController.java
+++ src/main/java/bzh.redge.keycloak/ApisController.java
@@ -1,12 +1,23 @@
 package bzh.redge.keycloak;
 
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.OidcKeycloakAccount;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.keycloak.representations.AccessToken;
+import org.keycloak.representations.IDToken;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@Slf4j
 public class ApisController {
 
     @RequestMapping(path = {"/", "/unsecured"})
@@ -31,7 +42,28 @@ public class ApisController {
     )
     @Secured({"ROLE_VIEWER"})
     public String userSecuredEndpoint(){
+
+        log.info("realm : " + getSecurityContext().getRealm());
+
+        IDToken token = getSecurityContext().getIdToken();
+
+        log.info("myEmail : " + token.getEmail());
+        log.info("myId : " + token.getId());
+
         return "This is an USER endpoint payload";
     }
 
+    private KeycloakSecurityContext getSecurityContext() {
+        ServletRequestAttributes requestAttributes = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes());
+        HttpServletRequest request = requestAttributes.getRequest();
+
+        KeycloakAuthenticationToken authenticationToken = (KeycloakAuthenticationToken) request.getUserPrincipal();
+        OidcKeycloakAccount oidcKeycloakAccount = authenticationToken.getAccount();
+        return oidcKeycloakAccount.getKeycloakSecurityContext();
+    }
+
+    private AccessToken getAccessToken() {
+        KeycloakSecurityContext securityContext = getSecurityContext();
+        return securityContext.getToken();
+    }
 }
--- src/main/java/bzh.redge.keycloak/SecurityConfiguration.java
+++ src/main/java/bzh.redge.keycloak/SecurityConfiguration.java
@@ -4,19 +4,16 @@ import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @KeycloakConfiguration
 @EnableGlobalMethodSecurity(
@@ -57,11 +54,11 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
         http.sessionManagement()
                 // use previously declared bean
                 .sessionAuthenticationStrategy(sessionAuthenticationStrategy())
-
                 // keycloak filters for securisation
                 .and()
                 .addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class)
                 .addFilterBefore(keycloakAuthenticationProcessingFilter(), X509AuthenticationFilter.class)
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
     }
+
 }
